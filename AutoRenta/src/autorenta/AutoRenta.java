2609382e2e9a93aaa926445da47a9d29e229e47d
EdgarOPG <eopg95@gmail.com>
2015-10-08 20:23:23 -0600

    Primeros frames del proyecto

---
+++ AutoRenta/src/autorenta/AutoRenta.java
@@ -0,0 +1,17 @@
+
+package autorenta;
+
+/**
+ *
+ * @author Edgar
+ */
+public class AutoRenta {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
